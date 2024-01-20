26af73d284bb8d5d3a86703f072fed2e853691db
DucHo <hohuuduc2709@gmail.com>
2024-01-21 00:11:21 +0700

    Draft

--- dist/Environment.js
+++ dist/Environment.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const path = "./etc/secrets/";
+const path = __dirname.slice(0, -4) + "etc/secrets/";
 const config = fs_1.default.readFileSync(path + "config.json", "utf8");
 const cert = fs_1.default.readFileSync(path + "cert.pem", "utf8");
 exports.default = { variables: JSON.parse(config), cert: cert };

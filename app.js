696e0cadfeceec236db20d35a81b5a8090c73176
Ho Huu Duc <66152949+27hohuuduc@users.noreply.github.com>
2024-01-21 23:13:57 +0700

    Draft

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 
 console.log(__dirname)
 
+console.log(fs.readdirSync(__dirname))
+
 console.log(fs.readdirSync(__dirname + "/etc/secrets/"))
 
 console.log(fs.readFileSync(__dirname + "/etc/secrets/config.json"))

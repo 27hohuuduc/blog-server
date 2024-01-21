9771111429137d6d6505cd3b9239e02e94e72cd8
Ho Huu Duc <66152949+27hohuuduc@users.noreply.github.com>
2024-01-22 00:08:38 +0700

    Draft

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const fs = require('fs')
 
-__dirname = __dirname.slice(0, -4)
-
-console.log(__dirname)
-
-console.log(fs.readdirSync(__dirname))
-
-console.log(fs.readdirSync(__dirname + "/etc/secrets/"))
-
-console.log(fs.readFileSync(__dirname + "/etc/secrets/config.json"))
+console.log(fs.readdirSync("."))
+console.log(fs.readdirSync("../"))
+console.log(fs.readdirSync("../../"))
+console.log(fs.readdirSync("../../../"))
+console.log(fs.readdirSync("../../../../"))
